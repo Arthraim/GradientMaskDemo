96c3c669c179fabe9fcc74cab249d0aefa4117ac
Arthur Wang <arthraim@gmail.com>
2016-01-05 14:40:04 +0800

    Initial Commit

---
+++ TestMask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMask
+//
+//  Created by Arthur Wang on 1/5/16.
+//  Copyright © 2016 Strikingly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
